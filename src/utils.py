7c9126d5bf8c29fa167dba9d610d0d68cb7d5202
Virender Chauhan <virchauhan657@gmail.com>
2024-03-24 16:11:18 +0530

    updated utils.py

--- src/utils.py
+++ src/utils.py
@@ -1,15 +1,55 @@
 import os
 import sys
 import joblib
+from src.logger import logging
 from src.exception import custom_exception
-
+from sklearn.metrics import r2_score
+from sklearn.model_selection import GridSearchCV
 # saving object
-def model_save(weights, file_path):
+def object_save(object, file_path):
     try:
         dir_path = os.path.dirname(file_path)
         os.makedirs(dir_path, exist_ok=True)
         with open(file_path, "wb") as f:
-            joblib.dump(weights,f)
+            joblib.dump(object,f)
     
     except Exception as e:
         raise custom_exception(e,sys)
+    
+
+def model_evaluation(x_train, x_test, y_train, y_test, models, parameters):
+    try:
+        logging.info("Model_evaluation started ")
+        detailed_report = {}
+                
+        for i in range(len(list(models))):
+            model = list(models.values())[i]
+            parameter =  parameters[list(models.keys())[i]]
+            grid_search = GridSearchCV(model, parameter, cv=4)
+            grid_search.fit(x_train, y_train)
+            
+            model.set_params(**grid_search.best_params_)
+            model.fit(x_train, y_train)
+            
+            y_train_pred = model.predict(x_train)
+            y_test_pred = model.predict(x_test)
+            
+            train_model_score = r2_score(y_train, y_train_pred)
+            test_model_score = r2_score(y_test, y_test_pred)
+            
+            detailed_report[list(models.keys())[i]] = test_model_score
+            return detailed_report            
+            
+    except Exception as e:
+        raise custom_exception(e,sys)
+    
+    
+
+def object_load(file_path):
+    try:
+        with open(file_path, "rb") as f:
+            return joblib.load(f)
+    except Exception as e:
+        raise custom_exception(e,sys)
+        
+    
